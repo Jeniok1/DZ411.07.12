453ec5db9f9ddef1de694becee96ad2a473e82f5
Евгений <baidak0310@gmail.com>
2024-12-13 21:20:31 +0200

    дз

---
+++ Untitled-1.py
@@ -0,0 +1,48 @@
+#a =int(input("введите число "))
+#b =int(input("введите число "))
+#print(a+b)
+#print(a*b)
+#print(a-b)
+
+#s =int(input("новое число "))
+#t =int(input("введите процент "))
+#print(s*(t/100))
+
+#a =int(input("введите число "))
+#x =int(input("ширина "))
+#print(i*x)
+
+#f =int(input("введите число "))
+#print(f*9/5+32)a =int(input("введите число "))
+
+#m =float(input("введите число "))
+#o =float(input("введите число "))
+#j =float(input("введите число "))
+#print((m+o+j)/3)
+
+#v =float(input("введите число "))
+#c =float(input("введите число "))
+#print(v**c)
+
+#p =3.14
+#d =float(input("введите число "))
+#print(p*d**2)
+#print(2*p*d)
+
+#грн =float(input("введите число "))
+#print(грн*0.024)
+#print(грн*0.023)
+#print(грн*0.00000024)
+
+#km =float(input("введите число "))
+#print(km*0.62)
+#print(km*0.53)
+
+#r =float(input("введите число "))
+#w =float(input("введите число "))
+#q =r
+#r =w
+#w =q
+#print(r)
+#print(w)
+#r,w = w,r
